0b456a8f0c4ff0c0542d5f3d0e18d779954a6973
John Slemmer <john.slemmer@gmail.com>
2016-04-26 13:48:04 -0400

    Making a foobar and hello world page.

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -4,13 +4,15 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 // route components
 import AppContainer from '../../ui/containers/AppContainer.jsx';
 import Layout from '../../ui/layouts/Layout.jsx';
-import Main from '../../ui/pages/Main.jsx';
+import HelloWorld from '../../ui/pages/HelloWorld.jsx';
+import Foobar from '../../ui/pages/Foobar.jsx';
 
 export const renderRoutes = () => (
     <Router history={browserHistory}>
-      <Route path="/" component={AppContainer}>
-        <Route component={Layout}>
-          <IndexRoute component={Main} />
+      <Route component={AppContainer}>
+        <Route path="/" component={Layout}>
+          <IndexRoute component={HelloWorld} />
+          <Route path="foobar" component={Foobar} />
         </Route>
       </Route>
     </Router>
---
+++ imports/ui/pages/Foobar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { IonContent } from 'reactionic';
+
+export default (props) => (
+  <IonContent customClasses="" {...props}>
+    <h2>Foobar!</h2>
+  </IonContent>
+);
